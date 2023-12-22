a4919366ae708dd2c3d78901f127c3086a7b7fcf
Nadai2010 <nadai2010@proton.me>
2023-12-22 10:18:42 +0000

    Update Sepolia

--- src/components/starknet-provider.tsx
+++ src/components/starknet-provider.tsx
@@ -1,38 +1,45 @@
-"use client";
-import { goerli, mainnet } from '@starknet-react/chains'
-import { argent, braavos, publicProvider, StarknetConfig, starkscan, useInjectedConnectors } from '@starknet-react/core'
-import { ArgentMobileConnector } from 'starknetkit/argentMobile'
-import { WebWalletConnector } from 'starknetkit/webwallet'
+'use client';
+import { sepolia, goerli, mainnet } from '@starknet-react/chains';
+import { argent, braavos, publicProvider, StarknetConfig, starkscan, useInjectedConnectors } from '@starknet-react/core';
+import { ArgentMobileConnector } from 'starknetkit/argentMobile';
+import { WebWalletConnector } from 'starknetkit/webwallet';
 
-const network = process.env.REACT_APP_NETWORK ?? 'goerli'
+const networkChains = {
+  sepolia,
+  goerli,
+  mainnet,
+};
+
+const networks = Object.keys(networkChains);
 
 // STARKNET
 
 interface StarknetProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export function StarknetProvider({ children }: StarknetProviderProps) {
   const { connectors: injected } = useInjectedConnectors({
     recommended: [argent(), braavos()],
     includeRecommended: 'always',
-  })
+  });
 
   const connectors = [
     ...injected,
     new WebWalletConnector({ url: 'https://web.argent.xyz' }),
     new ArgentMobileConnector(),
-  ]
+  ];
 
   return (
-    <StarknetConfig
-      connectors={connectors}
-      chains={[network === 'mainnet' ? mainnet : goerli]}
-      provider={publicProvider()}
-      explorer={starkscan}
-      autoConnect
-    >
-      {children}
-    </StarknetConfig>
-  )
+<StarknetConfig
+  connectors={connectors}
+  chains={networks.map((net) => networkChains[net as keyof typeof networkChains])}
+  provider={publicProvider()}
+  explorer={starkscan}
+  autoConnect
+>
+  {children}
+</StarknetConfig>
+
+  );
 }
